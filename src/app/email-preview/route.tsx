d0a4a22e34472ccb1f5fae7ddc8765937bcd5c44
Ezekwu <ezekwujerry@gmail.com>
2025-04-17 10:51:54 +0100

    removed email preview path

--- src/app/email-preview/route.tsx
+++
@@ -1,8 +0,0 @@
-// import { render } from '@react-email/render';
-// import { OrderCreatedTemplate } from '@/components/EmailTemplate/OrderCreatedTemplate';
-// export async function GET() {
-//   const html = await render(<OrderCreatedTemplate />);
-//   return  new Response(html, {
-//     headers: { 'Content-Type': 'text/html' },
-//   });
-// }
