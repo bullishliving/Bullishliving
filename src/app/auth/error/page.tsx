945dc88adc7ab3afac079d9acd949ecc6c1e0bf3
Ezekwu <ezekwujerry@gmail.com>
2025-04-17 11:06:36 +0100

    added suspense to useparams page

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,15 +1,22 @@
 'use client';
 
+import { Suspense } from 'react';
+
 import { useSearchParams } from 'next/navigation';
+import UiLoader from '@/components/ui/UiLoader';
 
 export default function AuthErrorPage() {
   const searchParams = useSearchParams();
   const message = searchParams.get('message') || 'An unknown error occurred.';
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold text-red-500">Authentication Error</h1>
-      <p className="mt-2 text-gray-700">{message}</p>
-    </div>
+    <Suspense fallback={<UiLoader/>}>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-2xl font-bold text-red-500">
+          Authentication Error
+        </h1>
+        <p className="mt-2 text-gray-700">{message}</p>
+      </div>
+    </Suspense>
   );
 }
